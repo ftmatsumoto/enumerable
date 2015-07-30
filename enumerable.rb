bd2fcf60925256502dcb15306536607a60a87fd0
ftmatsumoto <felipetmatsumoto@yahoo.com.br>
2015-07-30 11:13:56 -0300

    my_select funcionando

--- enumerable.rb
+++ enumerable.rb
@@ -6,5 +6,17 @@ module Enumerable
     end
   end
 
+  def my_each_with_index
+    for i in (0...self.length)
+      yield(self[i],i) if block_given?
+    end  	
+  end
+
+  def my_select
+  	
+  end
 
 end
+
+[1,2,3,4,5].select { |x| puts "#{x}" }
+[1,2,3,4,5].my_each_with_index { |x,y| puts "#{x},#{y}" }
